3476deba391a9645764743594d7a84c0b59c558b
Jotautas Treigys <jotautas.treigys@gmail.com>
2023-04-26 12:37:38 +0300

    added some mock data

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type note struct {
+	title string
+	body  string
+	tags  []tag
+}
+
+type tag struct {
+	title string
+	notes []note
+}
 
 func main() {
 	fmt.Println("Work in progress :)")
+	http.HandleFunc("/", notes)
+	http.ListenAndServe(":7890", nil)
+}
+
+func notes(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi")
 }
