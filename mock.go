9bf35931cfad290a0229badac8fb3e93989951c4
Jotautas Treigys <jotautas.treigys@gmail.com>
2023-05-01 12:15:24 +0300

    notes appear as cards

--- mock.go
+++ mock.go
@@ -33,6 +33,26 @@ var mock_notes = Page{
 			Body:  "It is a long established fact that a reader will be distracted by the readable content of a Page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web Page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
 			Tags: []Tag{
 				{
+					Title: "IT",
+					Notes: []Note{},
+				},
+			},
+		},
+		{
+			Title: "Note 4",
+			Body:  "It is a long established fact that a reader will be distracted by the readable content of a Page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web Page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+			Tags: []Tag{
+				{
+					Title: "IT",
+					Notes: []Note{},
+				},
+			},
+		},
+		{
+			Title: "Note 5",
+			Body:  "It is a long established fact that a reader will be distracted by the readable content of a Page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web Page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+			Tags: []Tag{
+				{
 					Title: "Funny",
 					Notes: []Note{},
 				},
